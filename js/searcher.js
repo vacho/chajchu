472192fb04dff3ae19286085eecbc379be7a03b5
vacho <vacho@koala-soft.com>
2015-08-15 17:03:49 -0400

    Mejoras al buscador

--- js/searcher.js
+++ js/searcher.js
@@ -4,5 +4,18 @@
         if ($(".messages").html().trim() == "") {
             $(".messages").css("display", "none");
         }
+
+        //@todo on press enter into searcher
+        /*
+        $("#busqueda").keyup(function(event){
+            if(event.keyCode == 13){
+                //$("#edit-offer").trigger("click");
+                $("#edit-offer").submit();
+                //document.getElementById('edit-offer').submit();
+                console.log("hola");
+                return false
+            }
+        });
+        */
     });
 })(jQuery);
